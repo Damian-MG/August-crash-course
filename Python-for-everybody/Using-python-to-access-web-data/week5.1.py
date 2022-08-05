0002ccb536fe05c82c1621cbc3516986fbfca606
Damian-MG <malenogon_f@hotmail.com>
2022-08-05 13:39:48 +0200

    hola

--- Python-for-everybody/Using-python-to-access-web-data/week5.1.py
+++ Python-for-everybody/Using-python-to-access-web-data/week5.1.py
@@ -5,3 +5,32 @@ Sample data: http://python-data.dr-chuck.net/comments_42.xml (Sum=2553)
 Actual data: http://python-data.dr-chuck.net/comments_353536.xml (Sum ends with 90)
 You do not need to save these files to your folder since your program will read the data directly from the URL. Note: Each student will have a distinct data url for the assignment - so only use your own data url for analysis.
 '''
+
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+#Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+count = 0
+sum = 0
+
+
+address = input('Enter location: ')
+if len(address) < 1 : address = 'http://python-data.dr-chuck.net/comments_42.xml'
+
+print('Retrieving ' + address)
+data = urllib.request.urlopen(address, context=ctx).read()
+
+tree = ET.fromstring(data)
+lst = tree.findall('.//count')
+
+for item in lst:
+    count += 1
+    sum += int(item.text)
+
+print('Count: ', count)
+print('Sum: ', sum)
