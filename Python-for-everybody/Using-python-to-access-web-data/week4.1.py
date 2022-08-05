a8a0a3ddd804e5c89bc1707a5426280340c77277
Damian-MG <malenogon_f@hotmail.com>
2022-08-05 12:59:01 +0200

    start week 5

--- Python-for-everybody/Using-python-to-access-web-data/week4.1.py
+++ Python-for-everybody/Using-python-to-access-web-data/week4.1.py
@@ -14,3 +14,40 @@ You do not need to save these files to your folder since your program
 will read the data directly from the URL. Note: Each student will have a
 distinct data url for the assignment - so only use your own data url for analysis.
 '''
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+#Initialize totalsum and count values to 0
+totalsum = 0
+count = 0
+
+#Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+#Set an input function for the url address and parse it through BeautifulSoup
+url = input('Enter - ')
+if len(url) < 1 : url = 'http://python-data.dr-chuck.net/comments_42.html'
+html = urllib.request.urlopen(url, context = ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+print(soup)
+
+#Convert the values in that class into text and append it to the numlist
+commentsclass = soup.select('span[class="comments"]')
+print(commentsclass)
+numlist = list()
+for comment in commentsclass:
+	numlist.append(comment.get_text())
+print(numlist)
+
+if len(numlist) > 0:
+	for num in numlist:
+		totalsum = totalsum + int(num)
+		count = count + 1
+#Print out the final values
+print("Count :",count)
+print("Total Sum :", totalsum)
+
--- Python-for-everybody/Using-python-to-access-web-data/week4.2.py
+++ Python-for-everybody/Using-python-to-access-web-data/week4.2.py
@@ -10,3 +10,48 @@ Actual problem: Start at: http://python-data.dr-chuck.net/known_by_Blanka.html
 Find the link at position 18 (the first name is 1). Follow that link. Repeat this process 7 times. The answer is the last name that you retrieve.
 Hint: The first character of the name of the last page that you will load is: L
 '''
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+#Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+#Set an input function for the url address and parse it through BeautifulSoup
+url = input('Enter URL : ')
+if len(url) < 1 : url = 'http://python-data.dr-chuck.net/known_by_Fikret.html'
+html = urllib.request.urlopen(url, context = ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+#Set up an input functions for the repeat
+countinp = input('Enter count : ')
+count = int(countinp)
+if count < 1 : count = 4
+
+#Set up an input function for the position
+positioninp = input('Enter position : ')
+position = int(positioninp)
+if position < 1 : position = 3 
+
+#Initialize currcount to 0
+currcount = 0
+
+#Repeat the loop for the required amount of repeats
+while currcount < count :
+
+	# Retrieve the url for the required position and print it
+	tags = soup('a')
+	for tag in tags:
+	    tag = tags[position - 1].get('href', None)
+	print("Retrieving : " + tag)
+
+        	#Update url and parse it through BeautifulSoup
+	url = tag
+	html = urllib.request.urlopen(url, context = ctx).read()
+	soup = BeautifulSoup(html, 'html.parser')
+
+	#Update currcount by 1 each step
+	currcount = currcount + 1
